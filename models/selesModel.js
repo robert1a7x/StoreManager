95e88c2157e737bc8ae438299f32df039d5f466e
Robert Nascimento <robert1a7x@gmail.com>
2022-01-26 16:40:33 -0300

    Tentanto corrigir o erro nos requisitos anteriores #2

--- models/selesModel.js
+++ models/selesModel.js
@@ -6,9 +6,8 @@ const create = async (sales) => {
 
   const createQuery = 'INSERT INTO sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)';
 
-  const promises = sales.map(async ({ productId, quantity }) => {
-    connection.execute(createQuery, [insertId, productId, quantity]);
-  });
+  const promises = sales.map(async ({ productId, quantity }) => 
+    connection.execute(createQuery, [insertId, productId, quantity]));
 
   await Promise.all(promises);
 
